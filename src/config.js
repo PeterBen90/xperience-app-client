0735ac2375bbc12b053ae3e707c718b88cbddf4e
Peter Benvenuto <peterbenvenuto90@yahoo.ca>
2018-04-17 23:06:21 -0400

    final deploy

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://obscure-mountain-54680.herokuapp.com/'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://obscure-mountain-54680.herokuapp.com/api'
